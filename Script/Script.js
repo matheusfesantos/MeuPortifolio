0009ce2486f014027bae90ddb3fdcab596ca1aff
Matheus Ferreira <matheusfgsantos31@gmail.com>
2024-10-21 21:20:17 -0300

    Att contras erros e mal intencionados

--- Script/Script.js
+++ Script/Script.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 const loadingTime = 4000;
 
 setTimeout(() => {
     window.location.href = 'Portifolio/index.html';
-=======
-const loadingTime = 4000;
-
-setTimeout(() => {
-    window.location.href = 'Portifolio/index.html';
->>>>>>> b2e768648c36e356a420eae0149ab094beb7c9d9
 }, loadingTime);
